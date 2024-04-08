f576ca9e1921a0094ad950c4993ec7559741b121
arunreddypadala <arunreddypadala@gmail.com>
2024-04-07 19:51:20 -0500

    Refactored Home About Us and Category code

--- src/test/java/gatlingdemostore/DemoStoreSimulation.java
+++ src/test/java/gatlingdemostore/DemoStoreSimulation.java
@@ -1,13 +1,12 @@
 package gatlingdemostore;
 
+import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.FeederBuilder;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
-import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
-import static io.gatling.javaapi.core.CoreDsl.css;
-import static io.gatling.javaapi.core.CoreDsl.regex;
-import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
 
 public class DemoStoreSimulation extends Simulation {
@@ -15,24 +14,59 @@ public class DemoStoreSimulation extends Simulation {
   private static final String DOMAIN = "demostore.gatling.io";
   private static final HttpProtocolBuilder HTTP_PROTOCOL = http.baseUrl("https://" + DOMAIN);
 
+  //Using external csv datasource for parameter values
+  private static final FeederBuilder<String> categoryFeeder =
+          csv("/Users/arunkumarreddy/Documents/Projects/Project-Gatling/gatling-demostore/src/test/resources/data/categoryDetails.csv")
+                  .random();
+
+  private static class CmsPage {
+
+    private static final ChainBuilder homePage =
+            exec(http("Load Home Page")
+                    .get("/")
+                    //Check to make sure home page loads correctly
+                    .check(regex("<title>Gatling Demo-Store</title>").exists())
+                    //Using css check to capture the csrf value and to be used in request 6
+                    .check(css("#_csrf", "content").saveAs("csrfValue")));
+
+
+    private static final ChainBuilder aboutUs =
+
+            exec(http("Load About Us")
+                    .get("/about-us")
+                    //Check to ensure the About Us title can be retrieved from the page
+                    .check(substring("About Us")));
+
+  }
+
+  private static class Catalog {
+    private static class Category {
+      private static final ChainBuilder view =
+              feed(categoryFeeder)
+                      .exec(
+                              //Parameterize the category type that we want to get
+                              http("Load Category Page - #{categoryName}")
+                                      .get("/category/#{categorySlug}")
+                                      //Check within the extracted page if the css selector CategoryName matches our parameter value
+                                      .check(css("#CategoryName").isEL("#{categoryName}")));
+
+
+    }
+
+  }
+
+
   {
 
     String uri1 = "demostore.gatling.io";
 
     ScenarioBuilder scn =
             scenario("DemostoreSimulation")
-                    .exec(http("Load Home Page")
-                            .get("/")
-                            //Check to make sure home page loads correctly
-                            .check(regex("<title>Gatling Demo-Store</title>").exists())
-                            //Using css check to capture the csrf value and to be used in request 6
-                            .check(css("#_csrf", "content").saveAs("csrfValue")))
+                    .exec(CmsPage.homePage)
                     .pause(2)
-                    .exec(http("Load About Us")
-                            .get("/about-us"))
+                    .exec(CmsPage.aboutUs)
                     .pause(2)
-                    .exec(http("Load Categories")
-                            .get("/category/all"))
+                    .exec(Catalog.Category.view)
                     .pause(2)
                     .exec(http("Load Product Details")
                             .get("/product/black-and-red-glasses"))
