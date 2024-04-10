70743aff480eeb37bf7b4b3fd039b1e434271971
arunreddypadala <arunreddypadala@gmail.com>
2024-04-09 20:41:05 -0500

    Refactored product, category and login code

--- src/test/java/gatlingdemostore/DemoStoreSimulation.java
+++ src/test/java/gatlingdemostore/DemoStoreSimulation.java
@@ -6,7 +6,15 @@ import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
-import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
+import static io.gatling.javaapi.core.CoreDsl.css;
+import static io.gatling.javaapi.core.CoreDsl.csv;
+import static io.gatling.javaapi.core.CoreDsl.exec;
+import static io.gatling.javaapi.core.CoreDsl.feed;
+import static io.gatling.javaapi.core.CoreDsl.jsonFile;
+import static io.gatling.javaapi.core.CoreDsl.regex;
+import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.core.CoreDsl.substring;
 import static io.gatling.javaapi.http.HttpDsl.http;
 
 public class DemoStoreSimulation extends Simulation {
@@ -18,6 +26,17 @@ public class DemoStoreSimulation extends Simulation {
   private static final FeederBuilder<String> categoryFeeder =
           csv("/Users/arunkumarreddy/Documents/Projects/Project-Gatling/gatling-demostore/src/test/resources/data/categoryDetails.csv")
                   .random();
+
+  private static final FeederBuilder<Object> JsonFeederProducts =
+          jsonFile("/Users/arunkumarreddy/Documents/Projects/Project-Gatling/gatling-demostore/src/test/resources/data/productDetails.json")
+                  .random();
+
+  private static final FeederBuilder<String> loginFeeder =
+          csv("/Users/arunkumarreddy/Documents/Projects/Project-Gatling/gatling-demostore/src/test/resources/data/loginDetails.csv")
+                  .circular();
+
+  private static final String uri1 = "demostore.gatling.io";
+
 
   private static class CmsPage {
 
@@ -53,12 +72,62 @@ public class DemoStoreSimulation extends Simulation {
 
     }
 
+    private static class Product {
+      private static final ChainBuilder view =
+
+              feed(JsonFeederProducts)
+                      .exec(
+                              http("Load product page - #{name}")
+                                      .get("/product/#{slug}")
+                                      .check(css("#ProductDescription").isEL("#{description}")));
+
+
+      private static final ChainBuilder add =
+
+              //Call the above product view chain builder and then process the below chain
+              exec(view).
+                      exec(
+                              http("Add Product to Cart")
+                                      .get("/cart/add/#{id}")
+                                      .check(substring("items in your cart")));
+
+
+    }
+
+  }
+
+  private static class Customer {
+    private static final ChainBuilder login =
+
+            feed(loginFeeder)
+                    .exec(
+                            http("Load login page")
+                                    .get("/login")
+                                    //Css check to see if we are in the login page
+                                    .check(substring("Username:")))
+                    .exec(
+                            http("Customer Login Action")
+                                    .post("http://" + uri1 + "/login")
+                                    .formParam("_csrf", "#{csrfValue}")
+                                    .formParam("username", "#{username}")
+                                    .formParam("password", "#{password}"));
+
+
+  }
+
+  private static class Checkout {
+    private static final ChainBuilder ViewCart =
+
+            exec(
+                    http("Viewing the Cart Page")
+                            .get("/cart/view"));
+
+
   }
 
 
   {
 
-    String uri1 = "demostore.gatling.io";
 
     ScenarioBuilder scn =
             scenario("DemostoreSimulation")
@@ -68,22 +137,11 @@ public class DemoStoreSimulation extends Simulation {
                     .pause(2)
                     .exec(Catalog.Category.view)
                     .pause(2)
-                    .exec(http("Load Product Details")
-                            .get("/product/black-and-red-glasses"))
+                    .exec(Catalog.Product.add)
                     .pause(2)
-                    .exec(http("Add Product to Cart")
-                            .get("/cart/add/19"))
+                    .exec(Checkout.ViewCart)
                     .pause(2)
-                    .exec(http("View Cart")
-                            .get("/cart/view"))
-                    .pause(2)
-                    .exec(
-                            http("Login User")
-                                    .post("http://" + uri1 + "/login")
-                                    //Remove the hardcoded csrf value and use the captured value in home page request
-                                    .formParam("_csrf", "#{csrfValue}")
-                                    .formParam("username", "user1")
-                                    .formParam("password", "pass"))
+                    .exec(Customer.login)
                     .pause(2)
                     .exec(http("Checkout Cart").get("http://" + uri1 + "/cart/checkout"));
 
